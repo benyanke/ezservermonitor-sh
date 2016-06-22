f3a7158fe5abe7e310a285a3cfc79380e7b3bef8
Ben Yanke <ben@benyanke.com>
2016-06-22 13:30:17 -0500

    Update liveDash.sh

--- liveDash.sh
+++ liveDash.sh
@@ -4,3 +4,19 @@
 ## Requires the "watch" command
 
 watch -n 30 --color ./eZServerMonitor.sh -Ca
+
+exit
+
+
+# Alternate method
+while true; do
+        clear;
+        echo;
+        ./eZServerMonitor.sh -Ca
+        sleep 5;
+done
+
+
+
+
+
